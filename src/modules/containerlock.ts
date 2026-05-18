b3434cb3e8a91126e4f42f6e900873c2ef6afa98
minidogg <117037081+minidogg@users.noreply.github.com>
2026-05-18 10:43:39 -0500

    container lock

---
+++ src/modules/containerlock.ts
@@ -0,0 +1,12 @@
+import {system,world,CustomCommandRegistry, CommandPermissionLevel, StartupEvent, CustomCommandParamType, CustomCommandOrigin, CommandResult, CustomCommandStatus, CustomCommand, Vector3, BlockComponent, BlockComponentTypes, ItemStack, ItemComponentTypes, GameMode, PlayerPermissionLevel} from '@minecraft/server'
+import { areItemsEqual } from '../lib/itemstack';
+
+export function loadModule(init: StartupEvent){
+  world.beforeEvents.playerInteractWithBlock.subscribe((e)=>{
+    let container = e.block.getComponent(BlockComponentTypes.Inventory)?.container
+    if(container&&(e.player.getGameMode()!=GameMode.Creative&&e.player.playerPermissionLevel!==PlayerPermissionLevel.Operator)&&e.block.below()?.typeId=="minecraft:bedrock"){
+      e.cancel = true
+      e.player.sendMessage("You must be in creative mode for this action because of the bedrock block below this container.")
+    }
+  })
+}
