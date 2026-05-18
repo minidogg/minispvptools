936dc8523f1c392d0ac856344dc1fc3b4fb71ddd
minidogg <117037081+minidogg@users.noreply.github.com>
2026-05-18 10:45:48 -0500

    forgor to load container lock module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import {system,world,CustomCommandRegistry, CommandPermissionLevel, StartupEvent, CustomCommandParamType, CustomCommandOrigin, CommandResult, CustomCommandStatus, CustomCommand, Vector3, BlockComponent, BlockComponentTypes, ItemStack, ItemComponentTypes} from '@minecraft/server'
 
 import * as copyitems from './modules/copyitems'
+import * as containerlock from './modules/containerlock'
+
 
 system.beforeEvents.startup.subscribe((init: StartupEvent) => {
   //todo: make a module auto loader using esbuild or something
   copyitems.loadModule(init)
+  containerlock.loadModule(init)
 })
